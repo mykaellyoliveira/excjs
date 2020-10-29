e44fd82b43a119083a1f774b3066b14f01b8775d
mykaellyoliveira <mykaelly.odc02@gmail.com>
2020-10-29 14:27:32 -0300

    Exemplos Switch

---
+++ exemplo-switch/index.js
@@ -0,0 +1,15 @@
+let nota ="B";
+
+switch(nota){
+  case "A":
+  console.log("Aluno aprovado com A.");
+  break;
+  case "B":
+  console.log("Aluno aprovado com B.")
+  break;
+  case "C":
+  console.log("Aluno aprovado com C.")
+  break;
+  default:
+  console.log("Aluno reprovado.");
+}
