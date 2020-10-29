4f194de7d69da1498e6537dd2a43ba9ba1c4f0a4
mykaellyoliveira <mykaelly.odc02@gmail.com>
2020-10-29 14:24:30 -0300

    Exercicios de função

---
+++ excsfuncoes/index.js
@@ -0,0 +1,45 @@
+const SOMA = 'somar';
+const SUB = 'subtrair';
+const MULT = 'multiplicar';
+const DIV = 'dividir';
+
+
+function calculadora(tipoCalculo, num, num2) {
+  let total=0;
+  switch(tipoCalculo) {
+    case SOMA:
+      total = soma(num, num2);
+      break;
+    case SUB:
+      total = subtrair(num, num2);
+      break
+    case MULT:
+      total = multiplicar(num, num2);
+      break
+    case DIV:
+      total = dividir(num, num2);
+      break
+    default:
+      return total;
+    break;
+  }
+  return total;
+}
+
+function soma(numero, numero2) {
+  return numero + numero2;
+}
+
+function subtrair(numero, numero2){
+  return numero - numero2;
+}
+
+function multiplicar(numero, numero2){
+  return numero * numero2;
+}
+
+function dividir(numero, numero2){
+  return numero / numero2;
+}
+
+console.log(calculadora(SUB, 5, 2))
